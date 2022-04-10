1c62d75c57a0f7e2486c88931b421884d83b7347
rtleib <rtleib@gmail.com>
2022-04-10 18:19:45 -0500

    added more questions

--- index.js
+++ index.js
@@ -14,13 +14,48 @@ const questions = [
   {
     type: 'input',
     name: 'description',
-    message: 'how would to describe your project?',
+    message: 'what would you like to describe in the description?',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'what would you like to describe in the installation?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'what would you like to describe in the usage?',
   },
   {
     type: 'list',
     name: 'license',
     message: 'what kind of license would you like?',
     choices: ['Mozilla', 'MIT', 'Boost', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'what would you like to describe in the contribution?',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'what would you like to describe in the tests?',
+  },
+  {
+    type: 'input',
+    name: 'questions',
+    message: 'what would you like to describe in the tests?',
+  },
+  {
+    type: 'input',
+    name: 'github username',
+    message: 'please enter your link to your GitHub profile',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'what is a good email address to contact you for additional questions?',
   },
 ];
 
